4941e0dc60e993746d105db9476a8d78f56bf450
Alisher778 <alisher.musurmonov89@gmail.com>
2018-12-15 15:29:29 +0500

    added some photos

--- js/custome.js
+++ js/custome.js
@@ -1,18 +1,33 @@
 let     index = 0;
 const   parentSlide = document.querySelectorAll('#slides-wrapper section');
 const width = window.innerWidth;
-function slide() {
-    parentSlide.forEach(function(sec) {
-        sec.style.display = 'none';
-    });
+// function slide() {
+//     parentSlide.forEach(function(sec) {
+//         sec.style.display = 'none';
+//     });
+//     if(index<parentSlide.length) {
+//         parentSlide[index].style.display = 'block';
+//         index++;
+//     } else {
+//         parentSlide[0].style.display = 'block';
+//         index = 0;
+//     }
+ 
+   
+// }
+
+// setInterval(slide, 2000);
+
+function slide2() {
     if(index<parentSlide.length) {
-        parentSlide[index].style.display = 'block';
+        const w = window.innerWidth*index;
+        parentSlide[0].style.marginLeft = -w+'px';
         index++;
     } else {
+        parentSlide[0].style.cssText = 'margin-left: 0; transition: 0';
         index = 0;
     }
  
    
 }
-
-setInterval(slide, 2000);
+setInterval(slide2, 20000);
